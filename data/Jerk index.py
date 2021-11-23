c7654e90e02aa7a8943d6812dbb3cc6004ef1dec
takumi-nishimura <clp13218@nitech.jp>
2021-11-23 23:31:53 +0900

    add search_data

--- data/Jerk index.py
+++ data/Jerk index.py
@@ -3,12 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 
-path = '/Users/sprout/OneDrive - 名古屋工業大学/学校/研究室/実験/予備実験/第4回ゼミ用/fusion/20211112_tsuruoka_tanada_partner_1.csv'
-df = pd.read_csv(path)
-filename = os.path.splitext(os.path.basename(path))[0]
-print("file:",filename)
-
-def Kojima_2006(tl,xl,yl,zl,j_x,j_y,j_z):
+def Teulings_1997(tl,xl,yl,zl,j_x,j_y,j_z):
 	sum_t = sum(tl)
 	dx = np.diff(xl,axis=0)
 	dy = np.diff(yl,axis=0)
@@ -20,60 +15,28 @@ def Kojima_2006(tl,xl,yl,zl,j_x,j_y,j_z):
 	jrk = np.linalg.norm(jrk_d,axis=1)
 	jrk_sq = np.power(jrk,2)
 	all_jrk_sq = np.sum(jrk_sq)
-	Jerk_index = sum_t**5/(d**2)*all_jrk_sq/2
+	Jerk_index = np.sqrt(1/2*all_jrk_sq*sum_t**5/d**2)
 	return Jerk_index
 
-def Teulings_1997():
-	pass
+def search_data(number,o_path):
+	n = number + 1
+	read_path = o_path + str(n) + '.csv'
+	df = pd.read_csv(read_path)
+	filename = os.path.splitext(os.path.basename(read_path))[0]
+	print(filename)
+	if 'x' in df.columns:
+		t = df.loc[:,'time']
+		x = df.loc[:,'x']
+		y = df.loc[:,'y']
+		z = df.loc[:,'z']
 
-if 'x' in df.columns:
-	t = df.loc[:,'time']
-	x = df.loc[:,'x']
-	y = df.loc[:,'y']
-	z = df.loc[:,'z']
+		jrk_x = df['jx'].values
+		jrk_y = df['jy'].values
+		jrk_z = df['jz'].values
+	return  t,x,y,z,jrk_x,jrk_y,jrk_z
 
-	jrk_x = df['jx'].values
-	jrk_y = df['jy'].values
-	jrk_z = df['jz'].values
-
-	Jrk_index = Kojima_2006(t,x,y,z,jrk_x,jrk_y,jrk_z)
-
-	print('robot :','{:.5g}'.format(Jrk_index))
-
-if 'x1' in df.columns:
-	tf = df.loc[:,'time'].sum()
-	x = df.loc[:,'x1']
-	y = df.loc[:,'y1']
-	z = df.loc[:,'z1']
-	dx = np.diff(x,axis=0)
-	dy = np.diff(y,axis=0)
-	dz = np.diff(z,axis=0)
-	pos_d = np.c_[dx,dy,dz]
-	norm = np.linalg.norm(pos_d,axis=1)
-	d = np.sum(norm)
-
-	jrk = df['j1'].values
-	jrk_sq = np.power(jrk,2)
-	all_jrk_sq = np.sum(jrk_sq)
-
-	Jerk_index = tf**5/(d**2)*all_jrk_sq/2
-	print('p_1 :','{:.5g}'.format(Jerk_index))
-
-if 'x2' in df.columns:
-	tf = df.loc[:,'time'].sum()
-	x = df.loc[:,'x2']
-	y = df.loc[:,'y2']
-	z = df.loc[:,'z2']
-	dx = np.diff(x,axis=0)
-	dy = np.diff(y,axis=0)
-	dz = np.diff(z,axis=0)
-	pos_d = np.c_[dx,dy,dz]
-	norm = np.linalg.norm(pos_d,axis=1)
-	d = np.sum(norm)
-
-	jrk = df['j2'].values
-	jrk_sq = np.power(jrk,2)
-	all_jrk_sq = np.sum(jrk_sq)
-
-	Jerk_index = tf**5/(d**2)*all_jrk_sq/2
-	print('p_2 :','{:.5g}'.format(Jerk_index))
+if __name__ == '__main__':
+	for i in range(5):
+		t,x,y,z,jrk_x,jrk_y,jrk_z = search_data(i,'/Users/sprout/OneDrive - 名古屋工業大学/学校/研究室/実験/予備実験/第4回ゼミ用/solo/20211112_tsuruoka_solo_')
+		Jrk_index = Teulings_1997(t,x,y,z,jrk_x,jrk_y,jrk_z)
+		print('robot :','{:.5g}'.format(Jrk_index))
